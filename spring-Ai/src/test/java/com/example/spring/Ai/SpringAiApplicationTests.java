cac1625eab7bb5e399e420474369c1861dc0b616
moin uddin <s.m.moin2004@gmail.com>
2025-08-03 21:27:25 +0500

    project structure

---
+++ spring-Ai/src/test/java/com/example/spring/Ai/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.Ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
